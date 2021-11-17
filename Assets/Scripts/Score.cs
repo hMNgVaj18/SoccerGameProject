4467493cd5df41b609cc554ee68683079588ce0d
hMNgVaj18 <92122999+hMNgVaj18@users.noreply.github.com>
2021-11-17 12:23:16 -0600

    Add empty project

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text ScoreText;
+    public Text TimerText;
+    public int MatchTime = 120;
+    private float StartTime = 0;
+    private int Scores = 0;
+    private bool MatchActive = false;
+
+    void Start()
+    {
+        SetTimeDisplay(MatchTime);
+        StartTime = Time.time;
+        MatchActive = true;
+
+    }
+    public void IncrementScore()
+    {
+        if (MatchActive)
+        {
+            Scores++;
+            ScoreText.text = Scores.ToString() + " - ";
+        }
+    }
+    
+    void Update()
+    {
+        if (Time.time-StartTime < MatchTime)
+        {
+            float ElapseTime = Time.time - StartTime;
+            SetTimeDisplay(MatchTime - ElapseTime);
+
+        }
+        else
+        {
+            MatchActive = false;
+            SetTimeDisplay(0);
+        }
+        
+    }
+
+    private void SetTimeDisplay(float TimeDisplay)
+    {
+        TimerText.text = "Time: " + GetTimeDisplay(TimeDisplay);
+
+    }
+
+    private string GetTimeDisplay(float TimeToShow)
+    {
+        int SecondsToShow = Mathf.CeilToInt(TimeToShow);
+        int Seconds = SecondsToShow % 60;
+        string SecondsDisplay = (Seconds < 10) ? "0" + Seconds.ToString(): Seconds.ToString();
+        int Minutes = (SecondsToShow - Seconds) / 60;
+        return Minutes.ToString() + ":" + SecondsDisplay ;
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/SoccerBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : MonoBehaviour
+{
+    public Score Controller;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {      
+        if (other.gameObject.tag == "Goal")
+        {
+            Debug.Log("Goal");
+            Controller.IncrementScore();
+            
+        }
+
+    }
+}
